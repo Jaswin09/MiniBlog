0bd7d6de3987cfb6df17b1aac84f54dd3d659f51
Jaswin09 <56599118+Jaswin09@users.noreply.github.com>
2021-05-05 20:29:30 +0530

    make it better

--- routes/user.js
+++ routes/user.js
@@ -75,4 +75,15 @@ router.put('/updatepic',requirelogin,(req,res)=>{
     })
 })
 
+router.post('/searchusers',(req,res)=>{
+    let userPattern= new RegExp("^"+req.body.query)
+    User.find({email:{$regex:userPattern}})
+    .select("_id email")
+    .then(user=>{
+        res.json({user})
+    }).catch(err=>{
+        console.log(err)
+    })
+})
+
 module.exports=router
